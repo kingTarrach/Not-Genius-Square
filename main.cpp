416d82817191bca9086d8054c2222d12aa88661b
Emma Johnson <97130396+emmaebjohnson@users.noreply.github.com>
2022-04-20 16:19:29 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
+#include <SFML/Graphics.hpp>
 
-
-int main(void)
+int main()
 {
-hshshs
-  
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Magenta);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
 }
