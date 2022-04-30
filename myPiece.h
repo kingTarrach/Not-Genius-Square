ba46ad476331682fedb69450321482142336aa94
kingTarrach <98366129+kingTarrach@users.noreply.github.com>
2022-04-30 16:43:06 -0700

    Update myPiece.h

--- myPiece.h
+++ myPiece.h
@@ -8,7 +8,7 @@ class myPiece : public sf::Drawable, public sf::Transformable {
 public:
 
 	void load(int pieceNumber, sf::Vector2f position, int size);
-	bool inBounds(sf::Vector2f position);
+	bool isOverlapping(sf::Vector2f position);
 	sf::Vector2f newPosition(sf::Vector2f mousePosition);
 	int getSize() {
 		return this->size;
