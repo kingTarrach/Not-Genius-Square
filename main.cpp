241afd3fb9e8db950d5003203bf4f280dbf2de9f
kingTarrach <98366129+kingTarrach@users.noreply.github.com>
2022-04-30 08:37:34 -0700

    Add files via upload
    Fuck this shit

--- main.cpp
+++ main.cpp
@@ -1,39 +1,93 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <Windows.h>
 
-#include "TestFunctions.h"
-
+#include "menu.h"
+#include "myPiece.h"
+using sf::Vector2f;
+using sf::Color;
 
 int main()
 {
-    //sf::RenderWindow window(sf::VideoMode(500, 500), "SFML works!");
 
-    ////Demo Shape Parameters are (shapeNumber, Vector2f position)
-    ////ShapeNumber determines what shape it is (will post shape values)
-    ////Position self explanatory
+	bool movingPiece = false;
+	sf::Vector2u windowSize(1000, 800);
+	sf::RenderWindow window(sf::VideoMode(windowSize.x, windowSize.y), "Not Genuis Cube");
+	myPiece piece;
+	piece.load(6, sf::Vector2f(50, 50), 50);
+	std::cout << piece.getOrigin().x << ", " << piece.getOrigin().y << std::endl;
+	std::cout << piece.getPosition().x << ", " << piece.getPosition().y << std::endl;
+	
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event)) {
 
-    //sf::RectangleShape background(sf::Vector2f(500, 500));
-    //background.setFillColor(sf::Color::White);
-    //Shape Piece[9];
-    //Piece[0].loadTexture(1);
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+			if (event.type == sf::Event::KeyPressed) {
+				switch (event.key.code) {
+				case sf::Keyboard::A:
+					piece.setPosition(piece.getPosition().x - piece.getSize(), piece.getPosition().y);
+					std::cout << piece.getOrigin().x << ", " << piece.getOrigin().y << std::endl;
+					std::cout << piece.getPosition().x << ", " << piece.getPosition().y << std::endl;
+					break;
+				case sf::Keyboard::D:
+					piece.setPosition(piece.getPosition().x + piece.getSize(), piece.getPosition().y);
+					std::cout << piece.getOrigin().x << ", " << piece.getOrigin().y << std::endl;
+					std::cout << piece.getPosition().x << ", " << piece.getPosition().y << std::endl;
+					break;
+				case sf::Keyboard::W:
+					piece.setPosition(piece.getPosition().x, piece.getPosition().y - piece.getSize());
+					std::cout << piece.getOrigin().x << ", " << piece.getOrigin().y << std::endl;
+					std::cout << piece.getPosition().x << ", " << piece.getPosition().y << std::endl;
+					break;
+				case sf::Keyboard::S:
+					piece.setPosition(piece.getPosition().x, piece.getPosition().y + piece.getSize());
+					std::cout << piece.getOrigin().x << ", " << piece.getOrigin().y << std::endl;
+					std::cout << piece.getPosition().x << ", " << piece.getPosition().y << std::endl;
+					break;
+				}
+			}
+			if (event.type == sf::Event::KeyReleased) {
+				switch (event.key.code) {
+				case sf::Keyboard::Left:
+					std::cout << piece.getOrigin().x << ", " << piece.getOrigin().y << std::endl;
+					std::cout << piece.getPosition().x << ", " << piece.getPosition().y << std::endl;
+					piece.rotate(270);
+					//piece.setPosition(piece.getPosition().x + piece.getSize(), piece.getPosition().y + piece.getSize());
+					break;
+				case sf::Keyboard::Right:
+					std::cout << piece.getOrigin().x << ", " << piece.getOrigin().y << std::endl;
+					std::cout << piece.getPosition().x << ", " << piece.getPosition().y << std::endl;
+					piece.rotate(90);
+					//piece.setPosition(piece.getPosition().x, piece.getPosition().y + piece.getSize());
+					break;
+				}
+			}
+		}
+		
+		//std::cout << x << ", " << y << std::endl;
+		window.clear();
+		
 
+		//Get the mouse position relative to screen and convert to vector2f
+		sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
+		sf::Vector2f worldPosition = window.mapPixelToCoords(mousePosition);
 
-    //while (window.isOpen())
-    //{
-    //    sf::Event event;
-    //    while (window.pollEvent(event))
-    //    {
-    //        if (event.type == sf::Event::Closed)
-    //            window.close();
-    //    }
+		//std::cout << piece.getOrigin().x << ", " << piece.getOrigin().y << std::endl;
+		//std::cout << mousePosition.x << ", " << mousePosition.y << std::endl;
+		//std::cout << piece.getPosition().x << ", " << piece.getPosition().y << std::endl;
 
-    //    window.clear();
-    //    window.draw(background);
-    //    window.draw(Piece[0]);
-    //    window.display();
+		
 
-    //}
-
-    //return 0;
-
-    TestFunctions test;
-    test.testShapePrint();
+		if (movingPiece) {
+			piece.setPosition(piece.newPosition(worldPosition));
+		}
+		window.draw(piece);
+		window.display();		
+		
+	}
+	return 0;
 }
---
+++ myPiece.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+class myPiece : public sf::Drawable, public sf::Transformable {
+
+public:
+
+	void load(int pieceNumber, sf::Vector2f position, int size);
+	bool inBounds(sf::Vector2f position);
+	sf::Vector2f newPosition(sf::Vector2f mousePosition);
+	int getSize() {
+		return this->size;
+	}
+	sf::VertexArray getPiece() {
+		return this->piece;
+	}
+
+
+private:
+
+	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const {
+
+		states.transform *= getTransform();
+
+		states.texture = &texture;
+
+
+		target.draw(piece, states);
+	}
+	void makeBox(int size, int vertexNumber, sf::Vector2f position);
+	bool checkSquare(int vertexNumber, sf::Vector2f position);
+
+	sf::VertexArray piece;
+	bool isRotated;
+	sf::Vector2f centerPosition();
+	sf::Vector2f generalBounds;
+	int size;
+	int shapeNumber;
+	sf::Texture texture;
+
+};
