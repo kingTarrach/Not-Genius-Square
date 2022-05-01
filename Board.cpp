3376ceaecdcd6bdde6c9d3fde84530ba73a0d712
Emma Johnson <97130396+emmaebjohnson@users.noreply.github.com>
2022-04-30 18:54:54 -0700

    Add files via upload

--- Board.cpp
+++ Board.cpp
@@ -1,15 +1,5 @@
 #include "Board.h"
 
-
-Board::Board(const Vector2f & pos, const Vector2f & size, const Color & color, bool isoccupied, bool ispeg) : sf::RectangleShape(size)
-{
-	this->setFillColor(color);
-	this->setPosition(pos);
-
-	this->isOccupied = isoccupied; 
-	this->isPeg = ispeg; 
-}
-
 bool Board::getisOccupied(void) const
 {
 	return this->isOccupied;
