f3410f58a085187a7be4e698332e2a0b400d017c
H Woods <93542377+hyazinthe@users.noreply.github.com>
2022-04-30 22:04:42 -0700

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,112 @@
+/*
+* File: Board.h()
+* Class: CptS 122 Spring 22
+* Date - FINAL VERS. 4/30/22
+* Assignment: PA9
+* Description: PLEASE SEE READ ME FILE
+*	This will contain the Board class
+*/
+
+/*File: Board.cpp()
+* Description: This will contain function definitions for the Board class
+*/
+
+/*File: Main.cpp()
+* Class: CptS 122 Spring 22
+* Date - FINAL VERS. 4/30/22
+* Assignment: PA9
+* Description: PLEASE SEE READ ME FILE
+*	This is our version of the game Genius Square adhering to the
+*	requirements of PA9.
+*/
+
+/*
+* File: PiecesArr.h()
+* Class: CptS 122 Spring 22
+* Date - FINAL VERS. 4/30/22
+* Assignment: PA9
+* Description: PLEASE SEE READ ME FILE
+*	This will contain the PiecesArr class
+*/
+
+/*File: PiecesArr.cpp()
+* Description: This will contain function definitions for the PiecesArr class
+*/
+
+/*
+* File: gameWorld.h()
+* Class: CptS 122 Spring 22
+* Date - FINAL VERS. 4/30/22
+* Assignment: PA9
+* Description: PLEASE SEE READ ME FILE
+*	This will contain the gameWorld class
+*/
+
+/*File: gameWorld.cpp()
+* Description: This will contain function definitions for the gameWorld class
+*/
+
+/*
+* File: menu.h()
+* Class: CptS 122 Spring 22
+* Date - FINAL VERS. 4/30/22
+* Assignment: PA9
+* Description: PLEASE SEE READ ME FILE
+*	This will contain the menu class
+*/
+
+/*File: menu.cpp()
+* Description: This will contain function definitions for the menu class
+*/
+
+/*
+* File: rules.h()
+* Class: CptS 122 Spring 22
+* Date - FINAL VERS. 4/30/22
+* Assignment: PA9
+* Description: PLEASE SEE READ ME FILE
+*	This will contain the rules class
+*/
+
+/*File: rules.cpp()
+* Description: This will contain function definitions for the rules class
+*/
+
+/*
+* File: selection.h()
+* Class: CptS 122 Spring 22
+* Date - FINAL VERS. 4/30/22
+* Assignment: PA9
+* Description: PLEASE SEE READ ME FILE
+*	This will contain the selection class
+*/
+
+/*File: selection.cpp()
+* Description: This will contain function definitions for the selection class
+*/
+
+/*
+* File: test.h()
+* Class: CptS 122 Spring 22
+* Date - FINAL VERS. 4/30/22
+* Assignment: PA9
+* Description: PLEASE SEE READ ME FILE
+*	This will contain the test class
+*/
+
+/*File: test.cpp()
+* Description: This will contain function definitions for the test class
+*/
+
+/*
+* File: myPiece.h()
+* Class: CptS 122 Spring 22
+* Date - FINAL VERS. 4/30/22
+* Assignment: PA9
+* Description: PLEASE SEE READ ME FILE
+*	This will contain the myPiece class
+*/
+
+/*File: myPiece.cpp()
+* Description: This will contain function definitions for the myPiece class
+*/
